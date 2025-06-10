0e903fb5571bd0a20aebb3b6cb759bab5c349aff
choi jeong hwan <since740@gmail.com>
2025-06-10 11:35:05 +0900

    first2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ProtectedRoute from './ProtectedRoute';
 
 export default function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={process.env.PUBLIC_URL || '/staysync_admin'}>
       <Routes>
         <Route path="/login" element={<AdminLogin />} />
         <Route
